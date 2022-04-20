22a9d036c0d1c41f5997fe672b578fad7350f75d
Zoneam <zoneam@gmail.com>
2022-04-20 16:52:19 -0700

    bootstrap integration and delete functionality

--- controllers/skills.js
+++ controllers/skills.js
@@ -4,7 +4,8 @@ module.exports = {
     index,
     show,
     new: newSkill,
-    create
+    create,
+    delete: deleteSkill
   };
   
   function index(req, res) {
@@ -26,5 +27,10 @@ module.exports = {
   function create(req, res) {
     console.log(req.body)
     Skill.create(req.body);
+    res.redirect('/skills');
+  }
+
+  function deleteSkill(req, res) {
+    Skill.deleteOne(req.params.id);
     res.redirect('/skills');
   }
